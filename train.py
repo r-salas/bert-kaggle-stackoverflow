e61217ca0faf7cdca0b2fae61fada61f0b720e97
Rubén Salas <ruben.sls17@gmail.com>
2021-12-08 03:08:05 +0100

    Save checkpoint

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from model import StackOverflowClassifier
 from dataset import StackOverflowDataModule
 
 
-def train(fpath: str, batch_size: int = 16, epochs: int = 10, num_workers: int = 0, seed: int = 0, gpus: int = -1,
+def train(data_fpath: str, output_fpath: str = "latest.ckpt", batch_size: int = 16, epochs: int = 10, num_workers: int = 0, seed: int = 0, gpus: int = -1,
           accelerator: Optional[str] = "ddp"):
     if not torch.cuda.is_available():
         gpus = None
@@ -24,11 +24,13 @@ def train(fpath: str, batch_size: int = 16, epochs: int = 10, num_workers: int =
     torch.manual_seed(seed)
 
     model = StackOverflowClassifier()
-    datamodule = StackOverflowDataModule(fpath, batch_size=batch_size, num_workers=num_workers, seed=seed)
+    datamodule = StackOverflowDataModule(data_fpath, batch_size=batch_size, num_workers=num_workers, seed=seed)
 
     trainer = pl.Trainer(gradient_clip_val=1.0, max_epochs=epochs, gpus=gpus, accelerator=accelerator)
 
     trainer.fit(model, datamodule=datamodule)
+
+    trainer.save_checkpoint(output_fpath)
 
 
 if __name__ == "__main__":
